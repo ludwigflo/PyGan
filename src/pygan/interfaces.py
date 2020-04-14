a64d63aa1a2fba534ff7ff63d71d9721e167c216
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-04-14 20:16:30 +0200

    Started implementation of an abstract class for gan training

--- src/pygan/interfaces.py
+++ src/pygan/interfaces.py
@@ -1,6 +1,8 @@
 from .utils import read_parameter_file
 from abc import ABC, abstractmethod
+from torch.optim import Adam
 from typing import Generator
+from . import ml_utils
 import torch.nn as nn
 import torch
 
@@ -17,125 +19,54 @@ class GanGenerator(ABC):
 
         Parameters
         ----------
-            noise: Noise tensor which is used by the generator in order to synthesize data.
+        noise: Noise tensor which is used by the generator in order to synthesize data.
 
         Returns
         -------
-            Dictionary, in which generated data samples are stored.
+        Dictionary, in which generated data samples are stored.
         """
         raise NotImplementedError
 
-class CondGenerator(ABC):
+
+class GanDiscriminator(ABC):
     """
-    Abstract class for conditional GAN generators. These models aim to convert an input noise vector to an output, which
-    is conditioned on additional information.
-    """
-
-    @abstractmethod
-    def compute_noise_features(self, noise: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        """
-        Computes a latent representation from an input noise tensor.
-
-        Parameters
-        ----------
-            noise: Noise tensor which is used by the generator for synthesizing data.
-
-        Returns
-        -------
-            Latent space representation of the noise vector.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def compute_cond_features(self, cond_tensor: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        """
-        Computes a latent representation from an input, which represents information on which the output should be
-        conditioned.
-
-        Parameters
-        ----------
-            cond_tensor: Represents information on which the output should be conditioned.
-
-        Returns
-        -------
-            Latent space representation of the conditional tensor.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def generate_data_dict(self, noise: torch.Tensor, cond_tensor: torch.Tensor, *args, **kwargs) -> dict:
-        """
-        Method, which synthesizes data given an input noise vector and a tensor, on which the output should be
-        conditioned. The synthesized data is stored in a dictionary.
-
-        Parameters
-        ----------
-            noise: Noise tensor which is used by the generator in order to synthesize data.
-            cond_tensor: Data, on which the generated output should be conditioned.
-
-        Returns
-        -------
-            Dictionary, in which generated data samples are stored.
-        """
-        raise NotImplementedError
-
-
-class CondDiscriminator(ABC):
-    """
-    Abstract class for conditional GAN discriminators. These models aim to classify input data into real or synthetic
-    data. Conditional discriminator use additional information for their decision which means, they condition their
-    decision on additional information like class labels.
+    Abstract class for standard GAN discriminator.
     """
 
     @abstractmethod
-    def compute_input_features(self, input_data: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+    def discriminate_data(self, data: torch.Tensor, *args, **kwargs) -> torch.Tensor:
         """
-        Computes a latent representation from an input data tensor.
+        Forwards data through the discriminator and predicts, whether the data is real or synthetic.
 
         Parameters
         ----------
-            input_data: Data tensor, which is classified into real or synthesized data.
+        data: Real or fake data, which should be discriminated.
 
         Returns
         -------
-            Latent space representation of the data vector.
+        pred: Prediction confidence for input samples (confidence of being real).
         """
         raise NotImplementedError
-
-    @abstractmethod
-    def compute_cond_features(self, cond_tensor: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        """
-        Computes a latent representation from an conditional input (e.g.class labels).
-
-        Parameters
-        ----------
-            cond_tensor: Represents information on which the final decision should be conditioned (e.g. class labels).
-
-        Returns
-        -------
-            Latent representation of the conditional tensor.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def predict_data(self, noise: torch.Tensor, cond_tensor: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        """
-        Method, which predicts whether provide data is real or synthetic. This method also utilizes additional data,
-        which is used to condition its prediction on.
-
-        Parameters
-        ----------
-            noise: Noise tensor which is classified by the discriminator into real or fake data.
-            cond_tensor: Data, on which the classification decision is conditioned.
-
-        Returns
-        -------
-            Dictionary, in which generated data samples are stored.
-        """
-        raise NotImplementedError
-
 
 class Gan(ABC):
+
+
+
+    @property
+    @abstractmethod
+    def generator(self):
+        """
+        Gan Generator.
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def discriminator(self):
+        """
+        Gan Discriminator.
+        """
+        raise NotImplementedError
 
     @abstractmethod
     def generate_data(self, noise: torch.Tensor, *args, **kwargs) -> dict:
@@ -169,42 +100,11 @@ class Gan(ABC):
         """
         raise NotImplementedError
 
-class CondGan(ABC):
 
-    @abstractmethod
-    def generate_data(self, noise: torch.Tensor, cond_tensor: torch.Tensor, *args, **kwargs) -> dict:
-        """
-        Synthesizes new data samples (based on an input noise tensor) and returns them in a consistent way with
-        dictionaries. The noise is forwarded through the generator with a self.forward(noise) call.
-
-        Parameters
-        ----------
-            noise: input, which is fed into the generator to create synthetic data.
-            cond_tensor: Represents information on which generated data should be conditioned (e.g. class labels).
-        Returns
-        -------
-            Dictionary, in which synthetic samples are stored.
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def discriminate_data(self, data: torch.Tensor, cond_tensor: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        """
-        Forwards data through the discriminator (self.discriminator(data) call) and predicts, whether the data is real
-        or synthetic.
-
-        Parameters
-        ----------
-            data: Real or fake data, which should be discriminated.
-            cond_tensor: Data, on which the classification decision is conditioned.
-
-        Returns
-        -------
-            pred: Prediction confidence for input samples (confidence of being real).
-        """
-        raise NotImplementedError
-
-
+# TODO: print training progress
+# TODO: create function train_epoch and move the training code there
+# TODO: create function validation epoch and implement function for validation
+# TODO: implement function for printing and logging the progress
 class GanTrainer(ABC):
     def __init__(self, gan_model: Gan, data_generator: Generator[dict], parameter_file: str) -> None:
         """
@@ -220,20 +120,77 @@ class GanTrainer(ABC):
         self.gan_model = gan_model
         self.data_generator = data_generator
         self.parameter_file = parameter_file
+        self.optimizer_list = []
 
     @abstractmethod
-    def discriminator_iteration(self):
+    def discriminator_iteration(self) -> dict:
         """
         Defines one training iteration of the discriminator module.
+
+        Returns
+        -------
+        A dictionary with the results of the training.
         """
         raise NotImplementedError
 
     @abstractmethod
-    def generator_iteration(self):
+    def generator_iteration(self) -> dict:
         """
         Defines one training iteration of the discriminator module.
+
+        Returns
+        -------
+        A dictionary with the results of the training.
         """
         raise NotImplementedError
+
+    @staticmethod
+    def init_experiment(params: dict) -> tuple:
+        """
+        Initializes a new experiment by creating an experiment directory and (optional) initializing tensorboard
+        loggers.
+
+        Parameters
+        ----------
+        params: Dictionary containing the settings for the experiment initialization.
+
+        Returns
+        -------
+        initialization: Tuple containing experiment directory and (optional) a list of tensorboard loggers.
+        """
+
+        # get the experiment settings
+        dir_root = params['experiment']['path']
+        tb_log = params['experiment']['tb_log']
+        if tb_log:
+            tb_log_names = ['generator', 'discriminator']
+        else:
+            tb_log_names = None
+
+        # perform the training
+        initialization = ml_utils.init_experiment_dir(dir_root, tb_log, tb_log_names)
+        return initialization
+
+    def prepare_optimizer(self, params: dict) -> None:
+        """
+        Prepares the optimizer for the gan training.
+
+        Parameters
+        ----------
+        params: Dictionary containing the parameters needed for the optimizers.
+
+        Returns
+        -------
+        optimizer_gen: Pytorch optimizer for the generator of the gan.
+        optimizer_dis: Pytorch optimizer for the discriminator of the gan.
+        """
+
+        lr_gen = params['training']['lr_gen']
+        lr_dis = params['training']['lr_dis']
+
+        optimizer_gen = Adam(self.gan_model.generator.parameters(), lr = lr_gen, betas = (0.9, 0.999))
+        optimizer_dis = Adam(self.gan_model.generator.parameters(), lr = lr_dis, betas = (0.9, 0.999))
+        self.optimizer_list = [optimizer_gen, optimizer_dis]
 
     def train(self) -> None:
         """
@@ -244,13 +201,44 @@ class GanTrainer(ABC):
         params = read_parameter_file(self.parameter_file)
         assert 'training' in params, "Parameter file needs to contain the key 'training', " \
                                      "in which the training configurations are defined "
+        assert 'experiment' in params, "Parameter file needs to contain the key 'experiment', " \
+                                       "in which the experiment configurations are defined "
+
         # extract the training settings
+        num_iterations = params['training']['num_iterations']
         num_dis_iter = params['training']['num_dis_iter']
         num_gen_iter = params['training']['num_gen_iter']
         num_epochs = params['training']['num_epochs']
-        lr_gen = params['training']['lr_gen']
-        lr_dis = params['training']['lr_dis']
         k_dis = params['training']['k_dis']
         k_gen = params['training']['k_gen']
 
-        # perform the training
+        # initialize a new experiment
+        init_experiment = self.init_experiment(params)
+        experiment_path = init_experiment[0]
+        if len(init_experiment) > 1:
+            tb_logger_list = init_experiment[1]
+        else:
+            tb_logger_list = None
+
+        # create the optimizer
+        self.prepare_optimizer(params)
+
+        # perform training
+        for epoch in range(num_epochs):
+
+            epoch_results = []
+            for iteration in range(num_iterations):
+
+                # train the discriminator
+                dis_results = []
+                for k in range(k_dis):
+                    results = self.discriminator_iteration()
+                    dis_results.append(results)
+
+                # train the discriminator
+                gen_results = []
+                for k in range(k_gen):
+                    results = self.generator_iteration()
+                    gen_results.append(results)
+
+            epoch_results.append((dis_results, gen_results))
