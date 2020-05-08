3b6c1850d66b5ffbd2da6b75b05b0fd7ce8b229d
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-05-08 17:26:00 +0200

    Increased image resolution to 128x128 Pixel

--- experiments/msg_gan/bach/main.py
+++ experiments/msg_gan/bach/main.py
@@ -10,32 +10,32 @@ import os
 # hyper parameters
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 parameter_file = 'parameters.yaml'
-batch_size = 64
-noise_size = 30
+batch_size = 256
+
+# initialize the model
+print('Initialize Model...')
+channels = (512, 256, 128, 64, 3)
+kernel_sizes = (4, 4, 4, 6)
+strides = (2, 2, 2, 2)
+paddings=(1, 1, 1, 2)
+spec_norm_gen = True
+spec_norm_dis = True
+org_size = (8, 8)
+latent_dim = 100
+model = msg_gan.GanModule(latent_dim = latent_dim, channels = channels, kernel_sizes = kernel_sizes, strides = strides,
+                          paddings = paddings, org_size = org_size, spec_norm_gen = spec_norm_gen,
+                          spec_norm_dis = spec_norm_dis)
+print('Done!\n')
 
 # create data loaders for train and test data
 print('Load data and create data loaders...')
 parameters = read_parameter_file(parameter_file)
-data_generator = ImgLoader(parameters, img_size=(3, 64, 64))
+data_generator = ImgLoader(parameters, img_size=(3, 128, 128))
+train_loader = data_loader(data_generator, batch_size=1, noise_size=latent_dim, size_tuple=(16, 32, 64), train_data=True)
+validation_loader = data_loader(data_generator, batch_size=1, noise_size=latent_dim,
+                                size_tuple=(16, 32, 64), train_data=False)
 print('Done!\n')
 
-# define ethe model parameters and initialize the model
-# print('Initialize the model')
-# channels=(256, 128, 64, 32, 1)
-# kernel_sizes=(4, 4, 4, 6)
-# latent_dim=noise_size
-# paddings=(1, 1, 2, 2)
-# strides=(2, 2, 2, 2)
-# spec_norm_dis=True
-# spec_norm_gen=True
-# coord_conv=False
-# org_size=(2, 2)
-#
-# model = msg_gan.GanModule(channels=channels, kernel_sizes=kernel_sizes, paddings=paddings, strides=strides,
-#                           spec_norm_dis=spec_norm_dis, spec_norm_gen=spec_norm_gen, coord_conv=coord_conv,
-#                           org_size=org_size, latent_dim=latent_dim)
-# print('Done!\n')
-#
-# # initialize the trainer
-# trainer = msg_gan.Trainer(model, train_loader, BCELoss, test_loader, parameter_file, device)
-# trainer.train()
+# initialize the trainer
+trainer = msg_gan.Trainer(model, train_loader, BCELoss, validation_loader, parameter_file, device)
+trainer.train()
